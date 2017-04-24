dd9883c3bbc367876a7a3158aa5d54f2010b139e
prakharchoudhary <prakhar13.cool@gmail.com>
2017-04-24 22:25:42 +0530

    Implemented User Login and SignUp

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ribbit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
+        'DIRS': ['ribbit/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- ribbit/urls.py
+++ ribbit/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from ribbit_app import views as rviews
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', rviews.index), # root
+    url(r'^login$', rviews.login_view), # login
+    url(r'^logout$', rviews.logout_view), # logout
+    url(r'^signup$', rviews.signup), # signup
 ]
